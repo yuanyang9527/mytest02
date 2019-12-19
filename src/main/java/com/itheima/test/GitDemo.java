082fdc41986d89bd27faeefcaa59ad2b0f01f13c
admin <849805073@qq.com>
2019-12-19 17:30:35 +0800

    提交本地仓库

---
+++ src/main/java/com/itheima/test/GitDemo.java
@@ -0,0 +1,14 @@
+package com.itheima.test;
+
+/**
+ * @Description TODO
+ * @Author Savage
+ * @Date 2019/12/19 17:23
+ */
+public class GitDemo {
+
+    public static void main(String[] args) {
+
+        System.out.println("Git测试...");
+    }
+}
